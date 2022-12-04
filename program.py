ddf372eeaa6d17b140d55aa4cc323131a23f3bc6
talalzeini <talalelzeini@gmail.com>
2022-12-04 01:34:02 -0800

    added feature to create posts for programs

---
+++ program.py
@@ -0,0 +1,111 @@
+from asyncore import read
+import os.path
+import os
+from pathlib import Path
+from PIL import Image, ImageDraw
+import aggdraw
+from io import BytesIO
+import random
+import compress as cs
+#
+# 
+# 
+# 
+# 
+# 
+# 
+languages = ["c", "cpp", "java", "python"]
+# 
+# 
+# 
+#
+#
+def add_margin(pil_img, top, right, bottom, left, color):
+    width, height = pil_img.size
+    new_width = width + right + left
+    new_height = height + top + bottom
+    result = Image.new(pil_img.mode, (new_width, new_height), color)
+    result.paste(pil_img, (left, top))
+    return result
+#
+#
+#
+#
+#
+def get_file_extension(file_path):
+    split_tup = os.path.splitext(file_path)
+    file_extension = split_tup[1]
+    return file_extension
+#
+#
+#
+#
+# 
+def add_corners(image, radius):
+    mask = Image.new('L', image.size)
+    draw = aggdraw.Draw(mask)
+    brush = aggdraw.Brush('white')
+    width, height = mask.size
+    draw.pieslice((0, 0, radius*2, radius*2), 90, 180, 255, brush)
+    draw.pieslice((width - radius*2, 0, width, radius*2), 0, 90, None, brush)
+    draw.pieslice((0, height - radius * 2, radius *
+                   2, height), 180, 270, None, brush)
+    draw.pieslice((width - radius * 2, height - radius *
+                   2, width, height), 270, 360, None, brush)
+    draw.rectangle((radius, radius, width - radius, height - radius), brush)
+    draw.rectangle((radius, 0, width - radius, radius), brush)
+    draw.rectangle((0, radius, radius, height-radius), brush)
+    draw.rectangle((radius, height-radius, width-radius, height), brush)
+    draw.rectangle((width-radius, radius, width, height-radius), brush)
+    draw.flush()
+    image = image.convert('RGBA')
+    image.putalpha(mask)
+    return image
+#
+#
+#
+#
+#
+def make_square(im):
+    x, y = im.size
+    size = max(256, x, y)
+    new_im = Image.new('RGBA', (size, size), (255, 255, 255, 1))
+    new_im.paste(im, (int((size - x) / 2), int((size - y) / 2)))
+    return new_im
+#
+#
+#
+#
+#
+def round_images(image_path_name):
+    if get_file_extension(image_path_name) != ".png":
+        print("Invalid file type. Only PNG files are accepted.")
+        return
+    image = Image.open(image_path_name)
+    image = add_corners(image, 25)
+    image = make_square(image)
+    image.save(image_path_name)
+
+def create_missing_folders():
+    if not cs.is_existing("code/ready"):
+        os.makedirs("code/ready")
+    if not cs.is_existing("code/not-ready"):
+        os.makedirs("code/not-ready")
+#
+#
+#
+#
+#
+def get_code_ready(image_path):
+    round_images(image_path)
+    image = Image.open(image_path)
+    new_image = Image.new("RGBA", image.size, "WHITE")
+    new_image.paste(image, (0, 0), image)
+    new_image.convert('RGB')
+    final_image =  add_margin(new_image, 25, 25, 25, 25, (255, 255, 255))
+    final_image.save('code/ready/code-' + str(random.randint(0,100)) + ".png", quality=95)
+
+def create_code_for_post():
+    create_missing_folders()
+    file_name = input("Enter the name of the file that is not ready to be posted: ")
+    get_code_ready("code/not-ready/" + str(file_name))
---
+++ title.py
@@ -0,0 +1,72 @@
+from PIL import Image, ImageFont, ImageDraw
+from datetime import datetime
+import os 
+
+languages = ["c", "cpp", "cs", "go", "java", "javascript", "python", "tips"]
+titles_path = "titles/"
+font_size = 100 # 55
+font_family = "Archive.otf"
+top_word_position = 5
+middle_word_position = 4
+bottom_word_position = 3
+
+def get_current_date():
+    now = datetime.now()
+    result = now.strftime("%Y-%m-%d-%H-%M-%S")
+    return result
+
+def print_invalid():
+    print("\nInvalid Input")
+
+def validate_title_length(title_choice):
+    # if(len(title_choice) > 11 or len(title_choice) <= 2):
+    #     print_invalid()
+    #     print("\nTitle Is Too Long\n")
+    #     return False
+    title_split = title_choice.split(" ")
+    if(len(title_split) == 1):
+        title = " ".join(title_split[0].upper())
+    if(len(title_split) == 2):
+        title = (" ".join(title_split[0])).upper(
+        ) + "  " + (" ".join(title_split[1])).upper()
+    if(len(title_split) == 3):
+        title = (" ".join(title_split[0])).upper(
+        ) + "  " + (" ".join(title_split[1])).upper() + "  " + (" ".join(title_split[2])).upper()
+    return title
+
+
+def draw_this(img, text, position):
+    I1 = ImageDraw.Draw(img)
+    W, H = (img.width, img.height)
+    myFont = ImageFont.truetype(font_family, font_size)
+    w = I1.textlength(text, myFont)
+    h = 51
+    I1.text(((W-w)/2, (H-h)/int(position)), text,
+    font=myFont,  fill=(0, 0, 0),
+    stroke_fill="black")
+
+def draw_title(img, title):
+    splited_title = ''.join(title).split("  ")
+    length = len(splited_title)
+    if(length == 1):
+        draw_this(img, splited_title[0], middle_word_position)
+    elif(length == 2):
+        draw_this(img, splited_title[0], top_word_position)
+        draw_this(img, splited_title[1], bottom_word_position)
+    else:
+        print_invalid()
+        return
+
+def create_title_for_post():
+    existing = os.path.exists(titles_path)
+    if(not existing):
+        os.makedirs(titles_path)
+    title_choice = input("\nEnter the title of your post: ")
+    if(validate_title_length(title_choice)):
+        new_folder_name = get_current_date()
+        os.makedirs('titles/' + str(new_folder_name), exist_ok=True)
+        title = validate_title_length(title_choice)
+        for i in range(len(languages)):
+            img = Image.open("languages/" + languages[i] + ".png")
+            draw_title(img, title)
+            img.save("titles/" + str(new_folder_name) + "/title-" + languages[i] + ".png")
